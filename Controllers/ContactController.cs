c67dc89fb8ef68c77f35e2bbc4eb09b6e4a7f2ea
Igor Carvalho <igor.carvalho.ti@gmail.com>
2024-01-19 08:57:43 -0300

    contact controller

--- Controllers/ContactController.cs
+++ Controllers/ContactController.cs
@@ -5,13 +5,21 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using MVC.Models;
+using MVC.Context;
 
 namespace MVC.Controllers
 {
-    public class ContactController : Controller{
-        public IActionResult Index(){
-
-            return View();
+    public class ContactController : Controller {
+    
+        private readonly ContextTable? _context;
+    
+        public ContactController(ContextTable context)
+        {
+            _context = context;
         }
+        public IActionResult Index(){
+        return View();
     }
+}
 }
